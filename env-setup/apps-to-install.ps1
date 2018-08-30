23f09c26d3807b7547a42d1b825887a342105e82
Henrik Ljungdahl <henrik.ljungdahl@tretton37.com>
2018-08-30 13:56:11 +0200

    initial-commit

---
+++ env-setup/apps-to-install.ps1
@@ -0,0 +1,8 @@
+choco install git
+choco install notepadplusplus
+choco install spotify
+choco install dropbox
+choco install 7zip
+choco install GoogleChrome
+choco install fiddler4
+choco install resharper-pl
