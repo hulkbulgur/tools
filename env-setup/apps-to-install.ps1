992a046514e2bfa128a607af6c524b5e15eb1ac6
Henrik Ljungdahl <henrik.ljungdahl@tretton37.com>
2018-09-01 18:29:07 +0200

    Added more application

--- env-setup/apps-to-install.ps1
+++ env-setup/apps-to-install.ps1
@@ -1,8 +1,20 @@
+## FIRST install IIS as a feature in windows.
+## FIRST install Visual Studio
+##  Plugins: Re-attach. Title-bar grejen. Coloured output-grejen. MD-file reader, CodeMaid
+
+choco install keepass
+choco install keepass-keepasshttp
 choco install git
 choco install notepadplusplus
 choco install spotify
 choco install dropbox
 choco install 7zip
 choco install GoogleChrome
-choco install fiddler4
-choco install resharper-pl
+choco install fiddler
+choco install resharper-platform
+choco install mremoteng
+choco install nodejs
+choco install urlrewrite
+choco install postman
+choco install wunderlist
+choco install whatsapp
