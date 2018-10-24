2d22957a595540f0d74ddd503778e28d63bd77d8
Henrik Ljungdahl <henrik.ljungdahl@gmail.com>
2018-10-24 20:13:47 +0200

    Added setup-windows.ps1

---
+++ env-setup/setup-windows.ps1
@@ -0,0 +1,12 @@
+# Setup displays
+# Log in dropbox
+# Put keepass, chrome, spotify, visual studio (admin), vs code in quick start field
+# Remove edge from quick start field
+# Set chrome to default browser
+# Set keepasshttp always allow entries
+# create /code directory with structure
+# remove store from the quick start field
+# Chrome, turn off save passwords setting
+# Chrome set translation settings
+# Remove mail from the quick start field
+# Explorer: Show file extensions
