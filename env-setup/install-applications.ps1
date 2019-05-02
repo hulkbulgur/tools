87845190786e7fc5e7c4a722647adf05d5e9350d
Henrik Ljungdahl <henrik.ljungdahl@gmail.com>
2019-05-02 11:12:20 +0200

    Removed visual studio stuff ith '#' will be ignored, and an empty message aborts the commit.

--- env-setup/install-applications.ps1
+++ env-setup/install-applications.ps1
@@ -23,15 +23,6 @@ choco install nodejs
 choco install postman
 choco install wunderlist
 choco install whatsapp
-choco install visualstudio2017enterprise
-choco install visualstudio2017-workload-azure
-choco install visualstudio2017-workload-data
-choco install visualstudio2017-workload-manageddesktop
-choco install visualstudio2017-workload-netcoretools
-choco install visualstudio2017-workload-netcrossplat
-choco install visualstudio2017-workload-netweb
-choco install visualstudio2017-workload-office
-choco install visualstudio2017-workload-universal
 choco install resharper --pre
 choco install slack
 choco install vscode
@@ -40,18 +31,15 @@ choco install vlc
 choco install sql-server-management-studio
 choco install foxitreader
 choco install firefox
-
-# No working version of the following
-# choco install microsoftazurestorageexplorer --ignore-checksum
-# choco install azurestorageemulator
+choco install logparser
 
 ##  Visual studio plugins:
-# Github Extension https://marketplace.visualstudio.com/items?itemName=GitHub.GitHubExtensionforVisualStudio
-# CodeMaid https://marketplace.visualstudio.com/items?itemName=SteveCadwallader.CodeMaid
-# ReAttach https://marketplace.visualstudio.com/items?itemName=ErlandR.ReAttach
-# Customize Visual Studio Window Title https://marketplace.visualstudio.com/items?itemName=mayerwin.RenameVisualStudioWindowTitle
-# VsColorOutput https://marketplace.visualstudio.com/items?itemName=MikeWard-AnnArbor.VSColorOutput
-# Markdown Editor https://marketplace.visualstudio.com/items?itemName=MadsKristensen.MarkdownEditor
-# Add new file https://marketplace.visualstudio.com/items?itemName=MadsKristensen.AddNewFile
-# Open command line https://marketplace.visualstudio.com/items?itemName=MadsKristensen.OpenCommandLine
-# Productivity power tools
+# https://marketplace.visualstudio.com/items?itemName=GitHub.GitHubExtensionforVisualStudio
+# https://marketplace.visualstudio.com/items?itemName=SteveCadwallader.CodeMaid
+# https://marketplace.visualstudio.com/items?itemName=ErlandR.ReAttach
+# https://marketplace.visualstudio.com/items?itemName=mayerwin.RenameVisualStudioWindowTitle
+# https://marketplace.visualstudio.com/items?itemName=MikeWard-AnnArbor.VSColorOutput
+# https://marketplace.visualstudio.com/items?itemName=MadsKristensen.MarkdownEditor
+# https://marketplace.visualstudio.com/items?itemName=MadsKristensen.AddNewFile
+# https://marketplace.visualstudio.com/items?itemName=MadsKristensen.OpenCommandLine
+# https://marketplace.visualstudio.com/items?itemName=VisualStudioPlatformTeam.ProductivityPowerPack2017
